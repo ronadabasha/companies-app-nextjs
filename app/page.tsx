3fc27fa32ec74d329ea955f2252d0a2c030c2d88
Ronada <basharonada@gmail.com>
2024-09-08 12:12:07 +0200

    redirect

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,7 @@
+import { redirect } from "next/navigation";
+
 export default function Home() {
+  redirect(`/ceos`);
   return (
     <div className="items-center justify-items-center font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col"></main>
