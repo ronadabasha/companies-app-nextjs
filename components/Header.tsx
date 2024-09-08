42a1a36064a2541ba6d9cfa56eff2905bd0ff9db
Ronada <basharonada@gmail.com>
2024-09-08 12:33:16 +0200

    Menu - active item

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,3 +1,5 @@
+"use client";
+import { usePathname } from "next/navigation";
 import Link from "next/link";
 import { Companies, People } from "./icons";
 
@@ -15,6 +17,7 @@ const menuItems = [
 ];
 
 export default function Header() {
+  const pathname = usePathname();
   return (
     <header className="px-4 min-[1125px]:px-0 fixed bg-white w-full left-0 top-0 border-b border-[rgb(140 140 140 / .25)] z-10">
       <nav className="flex justify-between items-center max-w-[1112px] mx-auto">
@@ -26,9 +29,16 @@ export default function Header() {
             return (
               <li
                 key={item.label}
-                className="inline-block min-w-[80px] text-xs text-black opacity-90"
+                className="inline-block min-w-[80px] text-xs text-black"
               >
-                <Link href={item.path} className="flex flex-col items-center">
+                <Link
+                  href={item.path}
+                  className={`link ${
+                    pathname === item.path
+                      ? "border-black"
+                      : "border-transparent opacity-90"
+                  } border-b-2 flex flex-col items-center pb-[2px]`}
+                >
                   <span>{item.icon}</span>
                   <span>{item.label}</span>
                 </Link>
